9136de2f708674a24645be9b68fadd813b8e7d14
skxxy <xiaoyixiong2023@u.northwestern.edu>
2023-01-24 03:20:12 +0000

    completed table and insert value

--- kmdb.sql
+++ kmdb.sql
@@ -101,13 +101,92 @@
 
 -- Drop existing tables, so you'll start fresh each time this script is run.
 -- TODO!
-
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS studio;
+DROP TABLE IF EXISTS actors;
+DROP TABLE IF EXISTS character;
 -- Create new tables, according to your domain model
 -- TODO!
+CREATE TABLE movies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    year INTEGER,
+    rating TEXT,
+    studio_id INTEGER
+);
+CREATE TABLE studio (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT
+);
+CREATE TABLE actors (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    movies_id INTEGER
+);
+CREATE TABLE character (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    actors_id INTEGER,
+    movies_id INTEGER
+);
 
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
 -- TODO!
+INSERT INTO studio (name) VALUES ("Warner Bros.");
+INSERT INTO movies(title,year,rating,studio_id)
+VALUES (
+    "Batman Begins",         
+    2005,        
+    "PG-13",
+    1
+),
+(   "The Dark Knight",        
+    2008,          
+    "PG-13",
+    1
+),
+(   "The Dark Knight Rises",
+    2012,        
+    "PG-13",
+    1
+);
+INSERT INTO actors (first_name,last_name, movies_id)
+VALUES ("Christian", "Bale",1),
+("Michael", "Caine",1),
+("Liam","Neeson",1),
+("Katie", "Holmes",1),
+("Gary","Oldman",1),
+("Christian", "Bale",2),
+("Heath", "Ledger",2),
+("Aaron","Eckhart",2),
+("Michael","Caine",2),
+("Maggie","Gyllenhaal",2),
+("Christian","Bale",3),
+("Gary","Oldman",3),
+("Tom","Hardy",3),
+("Joseph","Gordon-Levitt",3),
+("Anne","Hathaway",3);
+
+INSERT INTO character (first_name,last_name, actors_id,movies_id)
+VALUES ("Bruce","Wayne",1,1),
+("Alfred", Null,2,1),
+("Ra's","Al Ghul",3,1),
+("Rachel", "Dawes",4,1),
+("Commissioner","Gordon",5,1),
+("Bruce", "Wayne",1,2),
+("Joker",Null,2,2),
+("Harvey","Dent",3,2),
+("Alfred",Null,4,2),
+("Rachel","Dawes",5,2),
+("Bruce","Wayne",1,3),
+("Commissioner","Gordon",2,3),
+("Bane",Null,3,3),
+("John","Blake",4,3),
+("Selina","Kyle",5,3);
+
 
 -- Prints a header for the movies output
 .print "Movies"
@@ -116,7 +195,7 @@
 
 -- The SQL statement for the movies output
 -- TODO!
-
+SELECT title,year,rating,studio_id 
 -- Prints a header for the cast output
 .print ""
 .print "Top Cast"
